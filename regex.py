e94aa8ce0650d1017c9269c5a12d35d448da29f5
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2024-11-25 12:33:21 +0330

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,7 +1,7 @@
 import re
 
-pattern_mobile = r"(?:0?9|\+?989)\d{2}\W?\d{3}\W?\d{4}"
-number = '9126548745'
-mobile_match = re.findall(pattern_mobile, number)
-if mobile_match:
-    print(number)
+pattern_na_code = r"^[0-9]{10}$"
+nc_number = '2846775482'
+nc_match = re.findall(pattern_na_code, nc_number)
+if nc_match:
+    print(nc_number)
